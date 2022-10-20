e26c12769a17a5b95bdb65e18c008f44becf3d3d
dclcs <dcl.outlook.1016@gmail.com>
2022-10-20 16:32:39 +0800

    Fix : 切换到mysql

--- Package.swift
+++ Package.swift
@@ -11,22 +11,19 @@ let package = Package(
         
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
-        .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.0"),
+        .package(url: "https://github.com/vapor/fluent-mysql-driver", from: "4.0.0"),
         .package(url: "https://github.com/vapor/leaf.git",from: "4.0.0")
     ],
     targets: [
         .target(
             name: "App",
             dependencies: [
-                .product(name: "Vapor", package: "vapor"),
                 .product(name: "Fluent", package: "fluent"),
-                .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
-                .product(name: "Leaf", package: "leaf")
+                .product(name: "FluentMySQLDriver", package: "fluent-mysql-driver"),
+                .product(name: "Leaf", package: "leaf"),
+                .product(name: "Vapor", package: "vapor"),
             ],
             swiftSettings: [
-                // Enable better optimizations when building in Release configuration. Despite the use of
-                // the `.unsafeFlags` construct required by SwiftPM, this flag is recommended for Release
-                // builds. See <https://github.com/swift-server/guides/blob/main/docs/building.md#building-for-production> for details.
                 .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
             ]
         ),
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,11 +1,16 @@
 import Vapor
 import Fluent
-import FluentSQLiteDriver
+import FluentMySQLDriver
 // configures your application
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-    app.databases.use(.sqlite(.memory), as: .sqlite)
+    app.databases.use(.mysql(
+        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+        username: Environment.get("DATABASE_USERNAME") ??  "root",
+        password: Environment.get("DATABASE_PASSWORD") ?? "12345678",
+        database: Environment.get("DATABASE_NAME") ?? "toban_database",
+        tlsConfiguration: .none), as: .mysql)
     
     app.middleware.use(
         FileMiddleware(publicDirectory: app.directory.publicDirectory)
